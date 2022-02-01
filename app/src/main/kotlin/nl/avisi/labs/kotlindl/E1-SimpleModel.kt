7cb9f7eb7e885c10d6123674a6318d67e2b727b5
Marcel van Heerdt <m.c.heerdt@avisi.nl>
2022-02-01 11:43:28 +0100

    Refactors E1

--- app/src/main/kotlin/nl/avisi/labs/kotlindl/E1-SimpleModel.kt
+++ app/src/main/kotlin/nl/avisi/labs/kotlindl/E1-SimpleModel.kt
@@ -1,5 +1,6 @@
 package nl.avisi.labs.kotlindl
 
+import org.jetbrains.kotlinx.dl.api.core.GraphTrainableModel
 import org.jetbrains.kotlinx.dl.api.core.Sequential
 import org.jetbrains.kotlinx.dl.api.core.activation.Activations
 import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.Conv2D
@@ -12,46 +13,63 @@ import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
 import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
 import org.jetbrains.kotlinx.dl.api.core.optimizer.ClipGradientByValue
 import org.jetbrains.kotlinx.dl.api.core.summary.printSummary
+import org.jetbrains.kotlinx.dl.dataset.OnHeapDataset
+
+object E1Constants {
+    const val IMAGE_SIZE = 28L
+    const val NUMBER_OF_CLASSES = 10
+}
 
 fun main() {
-    val imageSize = 28L
-    val numberOfClasses = 10
+    val model = createModel()
 
-    val model = Sequential.of(
-        Input(dims = longArrayOf(imageSize, imageSize, 1)),
-        Conv2D(filters = 6, kernelSize = longArrayOf(5, 5), activation = Activations.Selu),
-        AvgPool2D(poolSize = intArrayOf(1, 4, 4, 1)),
-        Flatten(),
-        Dense(outputSize = 20, activation = Activations.Selu),
-        Dense(outputSize = numberOfClasses, activation = Activations.Softmax)
-    )
-
-    val (trainVal, test) = org.jetbrains.kotlinx.dl.dataset.mnist()
-
-    val (train, validation) = trainVal.split(.9)
+    val (train, validation, test) = getTrainValidationTestDatasets(validationSplit = 0.1)
 
     model.use {
-        it.compile(
-            optimizer = Adam(clipGradient = ClipGradientByValue(0.1f)),
-            loss = Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS,
-            metric = Metrics.ACCURACY
-        )
-
-        it.printSummary()
-
-        it.fit(
-            trainingDataset = train,
-            validationDataset = validation,
-            epochs = 3,
-            trainBatchSize = 500,
-            validationBatchSize = 500
-        )
-
-        val testResults = it.evaluate(dataset = test, batchSize = 500)
-
-        val accuracy = testResults.metrics[Metrics.ACCURACY]
-        val loss = testResults.metrics[Metrics.ACCURACY]
-
-        println("Loss: $loss, accuracy: $accuracy")
+        it.trainUsingDatasets(train, validation)
+        it.evaluateUsingDataset(test)
     }
+}
+
+private fun createModel() = Sequential.of(
+    Input(dims = longArrayOf(E1Constants.IMAGE_SIZE, E1Constants.IMAGE_SIZE, 1)),
+    Conv2D(filters = 6, kernelSize = longArrayOf(5, 5), activation = Activations.Selu),
+    AvgPool2D(poolSize = intArrayOf(1, 4, 4, 1)),
+    Flatten(),
+    Dense(outputSize = 20, activation = Activations.Selu),
+    Dense(outputSize = E1Constants.NUMBER_OF_CLASSES, activation = Activations.Softmax)
+)
+
+private fun getTrainValidationTestDatasets(validationSplit: Double): Triple<OnHeapDataset, OnHeapDataset, OnHeapDataset> {
+    val (trainVal, test) = org.jetbrains.kotlinx.dl.dataset.mnist()
+
+    val (train, validation) = trainVal.split(1.0 - validationSplit)
+    return Triple(train, validation, test)
+}
+
+private fun GraphTrainableModel.trainUsingDatasets(trainData: OnHeapDataset, validationData: OnHeapDataset) {
+    this.compile(
+        optimizer = Adam(clipGradient = ClipGradientByValue(0.1f)),
+        loss = Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS,
+        metric = Metrics.ACCURACY
+    )
+
+    this.printSummary()
+
+    this.fit(
+        trainingDataset = trainData,
+        validationDataset = validationData,
+        epochs = 3,
+        trainBatchSize = 500,
+        validationBatchSize = 500
+    )
+}
+
+private fun GraphTrainableModel.evaluateUsingDataset(testData: OnHeapDataset) {
+    val testResults = this.evaluate(dataset = testData, batchSize = 500)
+
+    val accuracy = testResults.metrics[Metrics.ACCURACY]
+    val loss = testResults.lossValue
+
+    println("Loss: $loss, accuracy: $accuracy")
 }
