cf2f6ed960d81faece9787169efde779aedad6a1
Marcel van Heerdt <m.c.heerdt@avisi.nl>
2022-02-14 16:59:23 +0100

    E6 - Uses constant for image size

--- app/src/main/kotlin/nl/avisi/labs/kotlindl/E6-SequentialH5Model.kt
+++ app/src/main/kotlin/nl/avisi/labs/kotlindl/E6-SequentialH5Model.kt
@@ -17,6 +17,7 @@ object E6Constants {
     const val MODEL_CONFIG_PATH = "./models/custom_model_h5_simple/mnist.json"
     const val MODEL_WEIGHTS_PATH = "./models/custom_model_h5_simple/mnist.h5"
     const val IMAGE_RESOURCE_PATH = "mnist_3.jpg"
+    const val IMAGE_SIZE = 28
 }
 
 /*
@@ -29,7 +30,7 @@ model = tf.keras.models.Sequential([
  */
 fun main() {
     val image = ImageConverter.toBufferedImage(Sequential::class.java.classLoader.getResourceAsStream(E6Constants.IMAGE_RESOURCE_PATH)!!)
-        .resizeToGrayscale(28, 28)
+        .resizeToGrayscale(E6Constants.IMAGE_SIZE, E6Constants.IMAGE_SIZE)
     val imageArray = OnHeapDataset.toNormalizedVector((image.raster.dataBuffer as DataBufferByte).data)
     Sequential.loadModelConfiguration(File(E6Constants.MODEL_CONFIG_PATH)).use {
         it.compile(Adam(), Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS, Metrics.ACCURACY)
