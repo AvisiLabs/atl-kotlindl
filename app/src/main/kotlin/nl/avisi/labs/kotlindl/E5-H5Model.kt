a75d54e003a3b841b0f75721834ed22ec8c16977
Marcel van Heerdt <m.c.heerdt@avisi.nl>
2022-02-14 13:40:45 +0100

    E5 - Adds example for H5 file

---
+++ app/src/main/kotlin/nl/avisi/labs/kotlindl/E5-H5Model.kt
@@ -0,0 +1,33 @@
+package nl.avisi.labs.kotlindl
+
+import io.jhdf.HdfFile
+import org.jetbrains.kotlinx.dl.api.core.Functional
+import org.jetbrains.kotlinx.dl.api.core.Sequential
+import org.jetbrains.kotlinx.dl.api.core.loss.Losses
+import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
+import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
+import org.jetbrains.kotlinx.dl.api.inference.keras.loadWeights
+import org.jetbrains.kotlinx.dl.dataset.image.ImageConverter
+import java.io.File
+
+object E5Constants {
+    const val MODEL_CONFIG_PATH = "./custom_model_h5/model.json"
+    const val MODEL_PATH = "./custom_model_h5/model_checkpoint.h5"
+    const val IMAGE_RESOURCE_PATH = "beer.jpg"
+}
+
+/*
+Model that works perfectly fine with Keras does not load in KotlinDL due to
+failing layer pattern check in TensorFlow. Even after replacing forward slashes
+it still fails due to missing inbound nodes.
+
+Model (loaded directly from h5) works fine in Python with Keras
+ */
+fun main() {
+    val image = ImageConverter.toNormalizedFloatArray(Sequential::class.java.classLoader.getResourceAsStream(E5Constants.IMAGE_RESOURCE_PATH)!!)
+    Functional.loadModelConfiguration(File(E5Constants.MODEL_CONFIG_PATH)).use {
+        it.compile(Adam(), Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS, Metrics.ACCURACY)
+        it.loadWeights(HdfFile(File(E5Constants.MODEL_PATH)))
+        val predictions = it.predict(image)
+    }
+}
