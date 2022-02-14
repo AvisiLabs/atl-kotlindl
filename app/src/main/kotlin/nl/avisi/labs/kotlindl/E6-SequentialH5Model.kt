c421453d4b3e6133cd707148a89095be66c51295
Marcel van Heerdt <m.c.heerdt@avisi.nl>
2022-02-14 17:27:07 +0100

    E6 - Removes unnecessary resizing

--- app/src/main/kotlin/nl/avisi/labs/kotlindl/E6-SequentialH5Model.kt
+++ app/src/main/kotlin/nl/avisi/labs/kotlindl/E6-SequentialH5Model.kt
@@ -8,8 +8,6 @@ import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
 import org.jetbrains.kotlinx.dl.api.inference.keras.loadWeights
 import org.jetbrains.kotlinx.dl.dataset.OnHeapDataset
 import org.jetbrains.kotlinx.dl.dataset.image.ImageConverter
-import java.awt.Image
-import java.awt.image.BufferedImage
 import java.awt.image.DataBufferByte
 import java.io.File
 
@@ -17,7 +15,6 @@ object E6Constants {
     const val MODEL_CONFIG_PATH = "./models/custom_model_h5_simple/mnist.json"
     const val MODEL_WEIGHTS_PATH = "./models/custom_model_h5_simple/mnist.h5"
     const val IMAGE_RESOURCE_PATH = "mnist_3.jpg"
-    const val IMAGE_SIZE = 28
 }
 
 /*
@@ -30,7 +27,6 @@ model = tf.keras.models.Sequential([
  */
 fun main() {
     val image = ImageConverter.toBufferedImage(Sequential::class.java.classLoader.getResourceAsStream(E6Constants.IMAGE_RESOURCE_PATH)!!)
-        .resizeToGrayscale(E6Constants.IMAGE_SIZE, E6Constants.IMAGE_SIZE)
     val imageArray = OnHeapDataset.toNormalizedVector((image.raster.dataBuffer as DataBufferByte).data)
     Sequential.loadModelConfiguration(File(E6Constants.MODEL_CONFIG_PATH)).use {
         it.compile(Adam(), Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS, Metrics.ACCURACY)
@@ -38,15 +34,4 @@ fun main() {
         val prediction = it.predict(imageArray)
         println(prediction)
     }
-}
-
-fun BufferedImage.resizeToGrayscale(width: Int, height: Int): BufferedImage {
-    val scaledImage = this.getScaledInstance(width, height, Image.SCALE_SMOOTH)
-    val resizedBufferedImage = BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY)
-
-    val graphics = resizedBufferedImage.createGraphics()
-    graphics.drawImage(scaledImage, 0, 0, null)
-    graphics.dispose()
-
-    return resizedBufferedImage
 }
