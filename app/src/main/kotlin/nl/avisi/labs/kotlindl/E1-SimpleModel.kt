3a7a2584f9a760d411cf3ee772ea9a4eaa3865ca
Marcel van Heerdt <m.c.heerdt@avisi.nl>
2022-02-21 10:23:08 +0100

    E1 - Improve model for better results

--- app/src/main/kotlin/nl/avisi/labs/kotlindl/E1-SimpleModel.kt
+++ app/src/main/kotlin/nl/avisi/labs/kotlindl/E1-SimpleModel.kt
@@ -4,21 +4,28 @@ import org.jetbrains.kotlinx.dl.api.core.GraphTrainableModel
 import org.jetbrains.kotlinx.dl.api.core.Sequential
 import org.jetbrains.kotlinx.dl.api.core.activation.Activations
 import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.Conv2D
+import org.jetbrains.kotlinx.dl.api.core.layer.convolutional.ConvPadding
 import org.jetbrains.kotlinx.dl.api.core.layer.core.Dense
 import org.jetbrains.kotlinx.dl.api.core.layer.core.Input
 import org.jetbrains.kotlinx.dl.api.core.layer.pooling.AvgPool2D
+import org.jetbrains.kotlinx.dl.api.core.layer.regularization.Dropout
 import org.jetbrains.kotlinx.dl.api.core.layer.reshaping.Flatten
 import org.jetbrains.kotlinx.dl.api.core.loss.Losses
 import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
 import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
+import org.jetbrains.kotlinx.dl.api.core.regularizer.L2
 import org.jetbrains.kotlinx.dl.api.core.summary.printSummary
 import org.jetbrains.kotlinx.dl.dataset.OnHeapDataset
 import org.jetbrains.kotlinx.dl.dataset.mnist
+import kotlin.math.pow
 
 object E1Constants {
     const val IMAGE_SIZE = 28L
-    const val BATCH_SIZE = 500
     const val NUMBER_OF_CLASSES = 10
+    const val BASE_CONV_FILTERS = 32
+
+    const val BATCH_SIZE = 500
+    const val EPOCHS = 10
 }
 
 fun main() {
@@ -32,14 +39,30 @@ fun main() {
     }
 }
 
-private fun createModel() = Sequential.of(
-    Input(dims = longArrayOf(E1Constants.IMAGE_SIZE, E1Constants.IMAGE_SIZE, 1)),
-    Conv2D(filters = 6, kernelSize = longArrayOf(5, 5), activation = Activations.Relu),
-    AvgPool2D(poolSize = intArrayOf(1, 4, 4, 1)),
-    Flatten(),
-    Dense(outputSize = 20, activation = Activations.Relu),
-    Dense(outputSize = E1Constants.NUMBER_OF_CLASSES, activation = Activations.Softmax)
-)
+private fun createModel(): GraphTrainableModel {
+    val layers = buildList {
+        add(Input(dims = longArrayOf(E1Constants.IMAGE_SIZE, E1Constants.IMAGE_SIZE, 1)))
+
+        repeat(2) {
+            val numberOfFilters = E1Constants.BASE_CONV_FILTERS * 2.0.pow(it).toLong()
+            add(
+                Conv2D(
+                    filters = numberOfFilters,
+                    kernelSize = longArrayOf(3, 3),
+                    activation = Activations.Relu,
+                    padding = ConvPadding.SAME,
+                    kernelRegularizer = L2()
+                )
+            )
+            add(AvgPool2D(poolSize = intArrayOf(1, 2, 2, 1)))
+            add(Dropout(keepProbability = 0.7f))
+        }
+
+        add(Flatten())
+        add(Dense(outputSize = E1Constants.NUMBER_OF_CLASSES, activation = Activations.Softmax))
+    }
+    return Sequential.of(layers)
+}
 
 private fun getTrainValidationTestDatasets(validationSplit: Double): Triple<OnHeapDataset, OnHeapDataset, OnHeapDataset> {
     val (trainVal, test) = mnist()
@@ -60,7 +83,7 @@ private fun GraphTrainableModel.trainUsingDatasets(trainData: OnHeapDataset, val
     this.fit(
         trainingDataset = trainData,
         validationDataset = validationData,
-        epochs = 3,
+        epochs = E1Constants.EPOCHS,
         trainBatchSize = E1Constants.BATCH_SIZE,
         validationBatchSize = E1Constants.BATCH_SIZE
     )
